a826e49225d86bbe435bd577797995f6c93697d4
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-25 12:12:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Read me
+
+The original idea is from  
+https://medium.com/@dlwlsdn153/localhost-connection-denied-in-h2-database-396ee6f6f9bf
---
+++ src/main/java/com/colak/springsecuritytutorial/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.colak.springsecuritytutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    // http://localhost:8080/h2-console/
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // Required to avoid You don't have authorization to view this page. HTTP ERROR 403
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(antMatcher("/h2-console/**")).permitAll()
+                        .anyRequest().authenticated())
+                // X-Frame-Options is an element in the HTTP response header that informs whether rendering is possible in <frame>, <iframe>, <object>, etc.
+                // Required to show IFrame
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        ;
+
+        return http.build();
+    }
+}
